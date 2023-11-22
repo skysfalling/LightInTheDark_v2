34a0973c9301e0f0de6d8f36cbd9bb0e76d429f7
~skysfall1ng~ <skythecreative@gmail.com>
2023-11-21 23:05:49 -0800

    worldgeneration works :))

--- Assets/WORLD/Generation/WorldGeneration.cs
+++ Assets/WORLD/Generation/WorldGeneration.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,20 +16,20 @@ public class WorldGeneration : MonoBehaviour
     Vector3 fullsized_chunkDimensions; // fullsize Chunk Dimensions [ size units == meters ] >> calculated by chunkDimensions * cellSize
     int fullSized_chunkWidth;
     int fullSized_chunkHeight;
-
+    GameObject combinedMeshObject;
 
     private void Awake()
     {
         chunkDimensions = new Vector3Int(chunkWidthCellCount, chunkHeightCellCount, chunkWidthCellCount);
         fullsized_chunkDimensions = chunkDimensions * cellSize;
         fullSized_chunkWidth = chunkWidthCellCount * cellSize;
-        fullSized_chunkHeight = chunkHeightCellCount * cellSize;
-
+        fullSized_chunkHeight = chunkHeightCellCount * cellSize;  
     }
 
-
-    void Start()
+    [EasyButtons.Button]
+    void Generate()
     {
+        // Generate Random Path
         List<Mesh> meshes = new List<Mesh>();
         List<Vector3> positions = GenerateRandomWalkPositions(steps);
 
@@ -43,145 +44,29 @@ public class WorldGeneration : MonoBehaviour
         CreateCombinedMeshObject(combinedMesh);
     }
 
-    Mesh OffsetMesh(Mesh mesh, Vector3 position)
-    {
-        Vector3[] vertices = mesh.vertices;
-        for (int i = 0; i < vertices.Length; i++)
-        {
-            vertices[i] += position;
-        }
-        mesh.vertices = vertices;
-        return mesh;
-    }
-
-    Mesh CombineMeshes(List<Mesh> meshes)
-    {
-        float mergeThreshold = 0.01f;
-        List<Vector3> newVertices = new List<Vector3>();
-        List<int> newTriangles = new List<int>();
-
-        foreach (Mesh mesh in meshes)
-        {
-            int[] originalTriangles = mesh.triangles;
-            Vector3[] originalVertices = mesh.vertices;
-
-            Dictionary<int, int> vertexMap = new Dictionary<int, int>();
-
-            for (int i = 0; i < originalVertices.Length; i++)
-            {
-                Vector3 vertex = originalVertices[i];
-                bool isMerged = false;
-
-                for (int j = 0; j < newVertices.Count; j++)
-                {
-                    if (Vector3.Distance(vertex, newVertices[j]) < mergeThreshold)
-                    {
-                        isMerged = true;
-                        vertexMap[i] = j; // Map original vertex index to new merged index
-                        break;
-                    }
-                }
-
-                if (!isMerged)
-                {
-                    newVertices.Add(vertex);
-                    vertexMap[i] = newVertices.Count - 1;
-                }
-            }
-
-            foreach (int triangleIndex in originalTriangles)
-            {
-                if (vertexMap.ContainsKey(triangleIndex))
-                {
-                    newTriangles.Add(vertexMap[triangleIndex]); // Remap triangle indices
-                }
-                else
-                {
-                    Debug.LogError("Triangle index out of bounds: " + triangleIndex);
-                    return null; // Early exit if there's an error
-                }
-            }
-        }
-
-        Mesh combinedMesh = new Mesh();
-        combinedMesh.vertices = newVertices.ToArray();
-        combinedMesh.triangles = newTriangles.ToArray();
-
-        // Recalculate UVs based on the new vertices
-        Vector2[] newUVs = new Vector2[newVertices.Count];
-        for (int i = 0; i < newVertices.Count; i++)
-        {
-            Vector3 vertex = newVertices[i];
-            newUVs[i] = new Vector2(vertex.x, vertex.z);
-        }
-        combinedMesh.uv = newUVs;
-
-        combinedMesh.RecalculateBounds();
-        combinedMesh.RecalculateNormals();
-
-        return combinedMesh;
-    }
-
-    void CreateCombinedMeshObject(Mesh combinedMesh)
-    {
-        GameObject combinedObject = new GameObject("CombinedChunk");
-        combinedObject.AddComponent<MeshFilter>().mesh = combinedMesh;
-        combinedObject.AddComponent<MeshRenderer>().material = chunkMaterial;
-    }
-
-    List<Vector3> GenerateRandomWalkPositions(int steps)
-    {
-        List<Vector3> positions = new List<Vector3>();
-        Vector3 currentPos = Vector3.zero; // Starting position
-
-        for (int i = 0; i < steps; i++)
-        {
-            currentPos += RandomDirectionOnXZ() * fullSized_chunkWidth;
-            positions.Add(currentPos);
-        }
-
-        return positions;
-    }
-
-    Vector3 RandomDirectionOnXZ()
-    {
-        int choice = Random.Range(0, 4);
-        switch (choice)
-        {
-            case 0: return Vector3.forward;
-            case 1: return Vector3.back;
-            case 2: return Vector3.left;
-            case 3: return Vector3.right;
-            default: return Vector3.forward;
-        }
-    }
-
     Mesh CreateChunkMesh()
     {
         Mesh mesh = new Mesh();
 
         List<Vector3> vertices = new List<Vector3>();
         List<int> triangles = new List<int>();
+        List<Vector2> uvs = new List<Vector2>(); // Initialize UV list
 
         // Helper method to add vertices for a face
         // 'start' is the starting point of the face, 'u' and 'v' are the directions of the grid
-        void AddFace(Vector3 start, Vector3 u, Vector3 v, int uDivisions, int vDivisions, float size)
+
+        void AddFace(Vector3 start, Vector3 u, Vector3 v, int uDivisions, int vDivisions, Vector3 faceNormal)
         {
-            // Loop over each subdivision in the vertical direction
             for (int i = 0; i <= vDivisions; i++)
             {
-                // Calculate the current position along the vertical direction 'v'
-                Vector3 vPos = start + (i * size * v);
-
-                // Loop over each subdivision in the horizontal direction
                 for (int j = 0; j <= uDivisions; j++)
                 {
-                    // Calculate the current position along the horizontal direction 'u'
-                    Vector3 uPos = j * size * u;
+                    vertices.Add(start + (i * cellSize * v) + (j * cellSize * u));
 
-                    // Add the vertex at the current position (vPos + uPos)
-                    // This represents a point on the face grid
-                    vertices.Add(vPos + uPos);
+                    // Standard UV rectangle for each face
+                    float uCoord = 1 - (j / (float)uDivisions); // Flipped horizontally
+                    float vCoord = i / (float)vDivisions;
+                    uvs.Add(new Vector2(uCoord, vCoord));
                 }
             }
         }
@@ -191,37 +76,41 @@ public class WorldGeneration : MonoBehaviour
             return new Vector3(a.x * b.x, a.y * b.y, a.z * b.z);
         }
 
-
-
-        // << SIDEFACES >>
+        // << FACES >>
         // note** :: starts the face at -fullsized_chunkDimensions so that top of chunk is at 0
         // -- the chunks will be treated as a 'Generated Ground' to build upon
         Vector3 newFaceStartOffset = new Vector3(fullsized_chunkDimensions.x * 0.5f, -fullsized_chunkDimensions.y, fullsized_chunkDimensions.z * 0.5f);
 
         // Forward face
         Vector3 forwardFaceStartVertex = MultiplyVectors(newFaceStartOffset, new Vector3(-1, 1, 1));
-        AddFace(forwardFaceStartVertex, Vector3.right, Vector3.up, chunkDimensions.x, chunkDimensions.y, cellSize);
+        Vector3 forwardFaceNormal = Vector3.forward; // Normal for forward face
+        AddFace(forwardFaceStartVertex, Vector3.right, Vector3.up, chunkDimensions.x, chunkDimensions.y, forwardFaceNormal);
 
         // Back face
         Vector3 backFaceStartVertex = MultiplyVectors(newFaceStartOffset, new Vector3(1, 1, -1));
-        AddFace(backFaceStartVertex, Vector3.left, Vector3.up, chunkDimensions.x, chunkDimensions.y, cellSize);
+        Vector3 backFaceNormal = Vector3.back; // Normal for back face
+        AddFace(backFaceStartVertex, Vector3.left, Vector3.up, chunkDimensions.x, chunkDimensions.y, backFaceNormal);
 
         // Right face
         Vector3 rightFaceStartVertex = MultiplyVectors(newFaceStartOffset, new Vector3(-1, 1, -1));
-        AddFace(rightFaceStartVertex, Vector3.forward, Vector3.up, chunkDimensions.z, chunkDimensions.y, cellSize);
+        Vector3 rightFaceNormal = Vector3.right; // Normal for right face
+        AddFace(rightFaceStartVertex, Vector3.forward, Vector3.up, chunkDimensions.z, chunkDimensions.y, rightFaceNormal);
 
         // Left face
         Vector3 leftFaceStartVertex = MultiplyVectors(newFaceStartOffset, new Vector3(1, 1, 1));
-        AddFace(leftFaceStartVertex, Vector3.back, Vector3.up, chunkDimensions.z, chunkDimensions.y, cellSize);
+        Vector3 leftFaceNormal = Vector3.left; // Normal for left face
+        AddFace(leftFaceStartVertex, Vector3.back, Vector3.up, chunkDimensions.z, chunkDimensions.y, leftFaceNormal);
 
-        // << VERTICAL FACES >> 
         // Top face
         Vector3 topFaceStartVertex = MultiplyVectors(newFaceStartOffset, new Vector3(1, 0, -1));
-        AddFace(topFaceStartVertex, Vector3.left, Vector3.forward, chunkDimensions.x, chunkDimensions.z, cellSize);
+        Vector3 topFaceNormal = Vector3.up; // Normal for top face
+        AddFace(topFaceStartVertex, Vector3.left, Vector3.forward, chunkDimensions.x, chunkDimensions.z, topFaceNormal);
 
         // Bottom face
         Vector3 bottomFaceStartVertex = MultiplyVectors(newFaceStartOffset, new Vector3(-1, 1, -1));
-        AddFace(bottomFaceStartVertex, Vector3.right, Vector3.forward, chunkDimensions.x, chunkDimensions.z, cellSize);
+        Vector3 bottomFaceNormal = Vector3.down; // Normal for bottom face
+        AddFace(bottomFaceStartVertex, Vector3.right, Vector3.forward, chunkDimensions.x, chunkDimensions.z, bottomFaceNormal);
+
 
         // Helper method to dynamically generate triangles for a face
         void AddFaceTriangles(int faceStartIndex, int uDivisions, int vDivisions)
@@ -233,7 +122,7 @@ public class WorldGeneration : MonoBehaviour
                     int rowStart = faceStartIndex + i * (uDivisions + 1);
                     int nextRowStart = faceStartIndex + (i + 1) * (uDivisions + 1);
 
-                    int bottomLeft = rowStart + j; 
+                    int bottomLeft = rowStart + j;
                     int bottomRight = bottomLeft + 1;
                     int topLeft = nextRowStart + j;
                     int topRight = topLeft + 1;
@@ -241,13 +130,6 @@ public class WorldGeneration : MonoBehaviour
                     // Add two triangles for each square
                     List<int> newSquareMesh = new List<int>() { bottomLeft, topRight, topLeft, topRight, bottomLeft, bottomRight };
                     triangles.AddRange(newSquareMesh);
-
-                    string debug = "newsquare : ";
-                    for (int k = 0; k < newSquareMesh.Count; k++)
-                    {
-                        debug += newSquareMesh[k];
-                    }
-                    print(debug);
                 }
             }
         }
@@ -278,13 +160,115 @@ public class WorldGeneration : MonoBehaviour
         // Apply the vertices and triangles to the mesh
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
+        mesh.uv = uvs.ToArray();
 
         // Recalculate normals for proper lighting
         mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
 
-        // UV mapping can be updated as required
+
+        void OnDrawGizmos()
+        {
+            if (mesh == null) return;
+
+            Gizmos.color = Color.red;
+            for (int i = 0; i < mesh.vertices.Length; i++)
+            {
+                Vector3 vertex = transform.TransformPoint(mesh.vertices[i]);
+                Vector2 uv = mesh.uv[i];
+
+                // Draw a small line or dot at the vertex position
+                // Use the UV coordinates to offset the line or dot, so you can visualize the direction
+                Gizmos.DrawRay(vertex, new Vector3(uv.x, uv.y, 0) * 0.1f);
+            }
+        }
+
 
         return mesh;
     }
+
+    Mesh OffsetMesh(Mesh mesh, Vector3 position)
+    {
+        Vector3[] vertices = mesh.vertices;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] += position;
+        }
+        mesh.vertices = vertices;
+        return mesh;
+    }
+
+    Mesh CombineMeshes(List<Mesh> meshes)
+    {
+        List<Vector3> newVertices = new List<Vector3>();
+        List<int> newTriangles = new List<int>();
+        List<Vector2> newUVs = new List<Vector2>(); // Add a list for the new UVs
+
+        int vertexOffset = 0; // Keep track of the vertex offset
+
+        foreach (Mesh mesh in meshes)
+        {
+            newVertices.AddRange(mesh.vertices); // Add all vertices
+
+            // Add all UVs from the current mesh
+            newUVs.AddRange(mesh.uv);
+
+            // Add the triangles, adjusted by the current vertex offset
+            foreach (var tri in mesh.triangles)
+            {
+                newTriangles.Add(tri + vertexOffset);
+            }
+
+            // Update the vertex offset for the next mesh
+            vertexOffset += mesh.vertexCount;
+        }
+
+        Mesh combinedMesh = new Mesh();
+        combinedMesh.vertices = newVertices.ToArray();
+        combinedMesh.triangles = newTriangles.ToArray();
+        combinedMesh.uv = newUVs.ToArray(); // Set the combined UVs
+
+        combinedMesh.RecalculateBounds();
+        combinedMesh.RecalculateNormals();
+
+        return combinedMesh;
+    }
+
+    void CreateCombinedMeshObject(Mesh combinedMesh)
+    {
+        if (combinedMeshObject != null) { Destroy(combinedMeshObject); }
+
+        combinedMeshObject = new GameObject("CombinedChunk");
+        combinedMeshObject.AddComponent<MeshFilter>().mesh = combinedMesh;
+        combinedMeshObject.AddComponent<MeshRenderer>().material = chunkMaterial;
+    }
+
+    List<Vector3> GenerateRandomWalkPositions(int steps)
+    {
+        List<Vector3> positions = new List<Vector3>();
+        Vector3 currentPos = Vector3.zero; // Starting position
+
+        for (int i = 0; i < steps; i++)
+        {
+            currentPos += RandomDirectionOnXZ() * fullSized_chunkWidth;
+            positions.Add(currentPos);
+        }
+
+        return positions;
+    }
+
+    Vector3 RandomDirectionOnXZ()
+    {
+        int choice = UnityEngine.Random.Range(0, 4);
+        switch (choice)
+        {
+            case 0: return Vector3.forward;
+            case 1: return Vector3.back;
+            case 2: return Vector3.left;
+            case 3: return Vector3.right;
+            default: return Vector3.forward;
+        }
+    }
+
 }
 
